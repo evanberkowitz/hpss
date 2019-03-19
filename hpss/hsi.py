8ea8caaeb00e04ffb1498c7c7ded43d9e5e6da8f
Evan Berkowitz <git@evanberkowitz.com>
2019-03-19 16:20:44 +0100

    More simple hooks+stubs

--- hpss/hsi.py
+++ hpss/hsi.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 
+def directory(file):
+    return "/".join(file.split("/")[:-1])
+
 def tape_mkdir(tape_directory):
     result = subprocess.call(["hsi", "mkdir", "-p", tape_directory])
     if result == 0:
@@ -34,13 +37,10 @@ def disk_info(disk_file):
     except:
         raise ValueError("ls -l {} does not exist successfully.".format(disk_file))
 
-    print(output)
     # Parse ls -l info:
     size = output[4]
     date = output[5:8]
     return size, date
-
-    
 
 def ensure_path_exists(tape_file):
     tape_directory = "/".join(tape_file.split("/")[:-1])
@@ -49,7 +49,29 @@ def ensure_path_exists(tape_file):
 
 def cput(disk_file, tape_file, log_file=subprocess.DEVNULL):
     mkdir(tape_directory)
-    subprocess.call(["hsi", "cput", disk, tape], stdout=log_file, stderr=subprocess.STDOUT)
+    return subprocess.call(["hsi", "cput", disk, tape], stdout=log_file, stderr=subprocess.STDOUT)
+    
 
 def cget(disk, tape, log_file=subprocess.DEVNULL):
     subprocess.call(["hsi", "cget", tape, disk], stdout=log_file, stderr=subprocess.STDOUT)
+
+def update_file(disk_file, tape_file):
+    
+    try:
+        tape_size, tape_date = tape_info(tape_file)
+    except:
+        tape_mkdir(directory(tape_file))
+        return cput(disk_file, tape_file)
+        
+    disk_size, disk_date = disk_info(disk_file)
+    
+    # Compare dates, cput if disk is newer.
+    ...
+
+def update_dir(disk_dir, tape_dir):
+    # Loop over each file in disk_dir and update_file
+    ...
+
+def update(disk, tape):
+    # Dispatch to _file or _dir based on disk target type.
+    ...
