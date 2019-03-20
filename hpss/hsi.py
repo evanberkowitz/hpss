4c42c5aede5c3db58c87c71684f149a3a0ed9f20
Evan Berkowitz <e.berkowitz@fz-juelich.de>
2019-03-20 11:48:48 -0400

    Learn more about hsi
    HSI has some funny design choices.
    
    First, it does not understand absolute file paths.  So, the `cput`
    command has to be invoked after an `lcd` and `cd` rather than just with
    absolute paths.  This is insanity.  It also means that `cget` probably
    needs to be fixed similarly.
    
    ON THE OTHER HAND, `cput` is also smart enough to only update the tape
    if the disk holds a newer file.  That makes life much easier.  No need
    to manually do date comparisons etc.

--- hpss/hsi.py
+++ hpss/hsi.py
@@ -4,8 +4,11 @@ import subprocess
 def directory(file):
     return "/".join(file.split("/")[:-1])
 
+def filename(file):
+    return file.split("/")[-1]
+
 def tape_mkdir(tape_directory):
-    result = subprocess.call(["hsi", "mkdir", "-p", tape_directory])
+    result = subprocess.call(["hsi", "-P", "mkdir", "-p", tape_directory])
     if result == 0:
         return result
     else:
@@ -20,16 +23,17 @@ def disk_mkdir(disk_directory):
 
 def tape_info(tape_file):
     try:
-        output = subprocess.check_output(["hsi", "-P", "ls", "-l", "--time-style", "long-iso", tape_file]).decode('UTF-8').split()
+        output = subprocess.check_output(["hsi", "-P", "ls", "-l", tape_file]).decode('UTF-8').split()
+
+        print(output)
+        output = output[1:] # Throw away the first line that corresponds to the prompt.
+    
+        # Parse ls -l info:
+        size = output[4]
+        date = output[5:8]
+        return size, date
     except:
         raise ValueError("hsi -P ls -l {} does not exit successfully.".format(tape_file))
-
-    output = output[1:] # Throw away the first line that corresponds to the prompt.
-    
-    # Parse ls -l info:
-    size = output[4]
-    date = output[5:8]
-    return size, date
 
 def disk_info(disk_file):
     try:
@@ -44,34 +48,20 @@ def disk_info(disk_file):
 
 def ensure_path_exists(tape_file):
     tape_directory = "/".join(tape_file.split("/")[:-1])
-    tape_mkdir(tape_directory)
+    return tape_mkdir(tape_directory)
     
 
 def cput(disk_file, tape_file, log_file=subprocess.DEVNULL):
-    tape_mkdir(tape_directory)
-    return subprocess.call(["hsi", "cput", disk, tape], stdout=log_file, stderr=subprocess.STDOUT)
+    ensure_path_exists(tape_file)
+
+    disk_path=directory(disk_file)
+    tape_path=directory(tape_file)
     
+    command = ["hsi", "-P", "lcd {}; cd {}; cput {}".format(disk_path, tape_path, filename(disk_file))]
+    print(command)
+    return subprocess.call(command)
 
 def cget(disk, tape, log_file=subprocess.DEVNULL):
-    subprocess.call(["hsi", "cget", tape, disk], stdout=log_file, stderr=subprocess.STDOUT)
+    subprocess.call(["hsi", "-P", "cget", tape, disk], stdout=log_file, stderr=subprocess.STDOUT)
 
-def update_file(disk_file, tape_file):
-    
-    try:
-        tape_size, tape_date = tape_info(tape_file)
-    except:
-        tape_mkdir(directory(tape_file))
-        return cput(disk_file, tape_file)
-        
-    disk_size, disk_date = disk_info(disk_file)
-    
-    # Compare dates, cput if disk is newer.
-    ...
 
-def update_dir(disk_dir, tape_dir):
-    # Loop over each file in disk_dir and update_file
-    ...
-
-def update(disk, tape):
-    # Dispatch to _file or _dir based on disk target type.
-    ...
